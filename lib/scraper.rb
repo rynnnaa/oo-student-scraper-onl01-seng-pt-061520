a280d50fa23d48322b7d3d41734a86ce001e1c05
Ryna Truong <rynatruong@outlook.com>
2020-07-27 15:43:06 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,6 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(https://learn-co-curriculum.github.io/student-scraper-test-page/))
-    doc
   end
 
   def self.scrape_profile_page(profile_url)

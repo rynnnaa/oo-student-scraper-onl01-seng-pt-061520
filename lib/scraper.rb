f88bf258e176f638ddaf2842f9eface5278e6c82
Ryna Truong <rynatruong@outlook.com>
2020-07-27 16:17:45 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,7 +11,7 @@ class Scraper
         student_profile_link = "#{student.attr('href')}"
         student_location = student.css('.student-location').text
         student_name = student.css('.student-name').text
-        students << {:name student_name, location: student_location, profile_url: student_profile_link}
+        students << {name: student_name, location: student_location, profile_url: student_profile_link}
       end
     end
     students

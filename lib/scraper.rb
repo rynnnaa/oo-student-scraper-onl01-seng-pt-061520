9d4ff07298035f9b9f5547cade791c667efcb975
Ryna Truong <rynatruong@outlook.com>
2020-07-27 15:46:59 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    html = https://learn-co-curriculum.github.io/student-scraper-test-page/index.html
+    # html = https://learn-co-curriculum.github.io/student-scraper-test-page/index.html
     doc = Nokogiri::HTML(html)
   end
 
